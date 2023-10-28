4373d8616af0f78c2513337efb4c792d3f3cf447
TerraMonster5 <william.dargan@gmail.com>
2023-10-29 00:51:54 +0100

    Added insert_cascade

--- src/tk.py
+++ src/tk.py
@@ -7,3 +7,7 @@ class ToolBar(tk.Menu):
     def add_cascade(self, name: str, label: str, **kwargs) -> None:
         setattr(self, name, ToolBar(self, **kwargs))
         super().add_cascade(label=label, menu=getattr(self, name))
+
+    def insert_cascade(self, index: int, name: str, label: str, **kwargs) -> None:
+        setattr(self, name, ToolBar(self, **kwargs))
+        super().insert_cascade(index, label=label, menu=getattr(self, name))
