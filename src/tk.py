8698836a95fb6157f5e8f71d80cec466191797c4
Will <93338246+TerraMonster5@users.noreply.github.com>
2023-11-06 13:20:49 +0000

    Added exceptions

--- src/tk.py
+++ src/tk.py
@@ -4,10 +4,28 @@ class ToolBar(tk.Menu):
     def __init__(self, master=None, **kwargs) -> None:
         super().__init__(master, **kwargs)
 
-    def add_cascade(self, name: str, label: str, **kwargs) -> None:
-        setattr(self, name, ToolBar(self, **kwargs))
-        super().add_cascade(label=label, menu=getattr(self, name))
+    def add(self, kind, **kwargs):
+        if kind == "cascade":
+            if "name" in kwargs.keys():
+                self.add_cascade(kwargs["name"], kwargs["label"], **kwargs)
+            else:
+                raise TypeError
+        else:
+            super().add(kind, **kwargs)
 
-    def insert_cascade(self, index: int, name: str, label: str, **kwargs) -> None:
-        setattr(self, name, ToolBar(self, **kwargs))
-        super().insert_cascade(index, label=label, menu=getattr(self, name))
+    def add_cascade(self, name: str, *, menukwargs: dict, cascadekwargs: dict) -> None:
+        if name in ToolBar.__dict__.keys():
+            raise Exception
+        else:
+            setattr(self, name, ToolBar(self, **menukwargs))
+            super().add_cascade(menu=getattr(self, name), **cascadekwargs)
+
+    def insert_cascade(self, index: int, name: str, *, menukwargs: dict, cascadekwargs: dict) -> None:
+        if name in ToolBar.__dict__.keys():
+            raise Exception
+        else:
+            setattr(self, name, ToolBar(self, **menukwargs))
+            super().insert_cascade(index, menu=getattr(self, name), **cascadekwargs)
+    
+    def delete(self, index1, index2=None):
+        pass
