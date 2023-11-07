c7c67c8afbe861b1a418a589febe7bf8cb255bd3
TerraMonster5 <william.dargan@gmail.com>
2023-11-07 20:38:03 +0000

    Added default values for kwargs and began delete

--- src/stk.py
+++ src/stk.py
@@ -6,7 +6,13 @@ class ToolBar(tk.Menu):
 
     def add(self, kind: str, cnf={}, **kwargs):
         kwargs = cnf or kwargs
+
         if kind == "cascade":
+            if "menukw" not in kwargs.keys():
+                kwargs["menukw"] = {}
+            if "cascadekw" not in kwargs.keys():
+                kwargs["cascadekw"] = {}
+
             if "name" in kwargs.keys():
                 setattr(self, kwargs["name"], ToolBar(self, **kwargs["menukw"]))
                 super().add("cascade", menu=getattr(self, kwargs["name"]), **kwargs["cascadekw"])
@@ -17,14 +23,21 @@ class ToolBar(tk.Menu):
 
     def add_cascade(self, name: str, cnf={}, **kwargs):
         kwargs = cnf or kwargs
+
         if name in ToolBar.__dict__.keys():
             raise Exception
         else:
             self.add("cascade", name=name, **kwargs)
-    
+
     def insert(self, index, kind: str, cnf={}, **kwargs):
         kwargs = cnf or kwargs
+
         if kind == "cascade":
+            if "menukw" not in kwargs.keys():
+                kwargs["menukw"] = {}
+            if "cascadekw" not in kwargs.keys():
+                kwargs["cascadekw"] = {}
+
             if "name" in kwargs.keys():
                 setattr(self, kwargs["name"], ToolBar(self, **kwargs["menukw"]))
                 super().insert(index, "cascade", menu=getattr(self, kwargs["name"]), **kwargs["cascadekw"])
@@ -32,10 +45,21 @@ class ToolBar(tk.Menu):
                 raise TypeError
         else:
             super().insert(index, kind, **kwargs)
-    
+
     def insert_cascade(self, index, name: str, cnf={}, **kwargs):
         kwargs = cnf or kwargs
+
         if name in ToolBar.__dict__.keys():
             raise Exception
         else:
             self.insert(index, "cascade", name=name, **kwargs)
+
+    def delete(self, index1, index2=None):
+        if index2 is None:
+            index2 = index1
+
+        children = self.__dict__.keys() - ToolBar.__dict__.keys()
+
+        for i in children:
+            if isinstance(getattr(self, i), ToolBar):
+                pass
